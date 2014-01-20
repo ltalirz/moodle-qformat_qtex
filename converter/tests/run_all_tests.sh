d1c9de2034a3f7290ee2c49fc666399c3c34f4e1
Patrick Spettel <patrick.spettel@aon.at>
2014-01-20 15:58:42 +0100

    Changed 'run_all_tests.sh'. It now takes the url as a parameter.

--- converter/tests/run_all_tests.sh
+++ converter/tests/run_all_tests.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-read -p "Please enter the URL pointig to 'gui.php' " URL
+function usage {
+    printf "usage: %s <URL to 'gui.php'>" $(basename $0)
+    exit 1
+}
 
+nCommandLineArgs=$#
+if (( $nCommandLineArgs != 1 ))
+then
+    usage
+fi
+
+URL=$1
+#read -p "Please enter the URL pointig to 'gui.php' " URL
 
 for test in ./test*
 do
